c986782705a88f6b3c7db6e5719d72ab9511adca
david-forer <david.j.forer@gmail.com>
2020-01-20 18:09:37 +0900

    added controllers, views and layout. Can sigh up for reviews

---
+++ app/controllers/winelists_controller.rb
@@ -0,0 +1,12 @@
+class WinelistsController < ApplicationController
+
+#get wine reviews/new to create form 
+get '/winelists/new' do
+    erb :'/winereviews/new'
+
+end
+#post request to create a review
+#show page for winereviews
+#index route for all reviews
+
+end
---
+++ app/controllers/winereviews_controller.rb
@@ -0,0 +1,12 @@
+class WinereviewsController < ApplicationController
+
+#get wine reviews/new to create form 
+get '/winereviews/new' do
+    erb :'/winereviews/new'
+
+end
+#post request to create a review
+#show page for winereviews
+#index route for all reviews
+
+end
